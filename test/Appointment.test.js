947d8a118cb0a058be152806d63ab22a7ac09120
y_fukuda <colors92window@gmail.com>
2020-09-08 07:48:47 +0900

    Appointment renders the customer first name: refactoring

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -4,17 +4,23 @@ import ReactDOM from 'react-dom';
 import { Appointment } from '../src/Appointment';
 
 describe('Appointment', () => {
+  let customer;
+  let container;
+  const render = (conponent) => ReactDOM.render(conponent, container);
+
+  beforeEach(() => {
+    container = document.createElement('div');
+  });
+
   it('renders the customer first name', () => {
-    const customer = { firstName: 'Alice' };
-    const container = document.createElement('div');
-    ReactDOM.render(<Appointment customer={customer} />, container);
+    customer = { firstName: 'Alice' };
+    render(<Appointment customer={customer} />);
     expect(container.textContent).toMatch('Alice');
   });
 
   it('renders another customer first name', () => {
-    const customer = { firstName: 'Bob' };
-    const container = document.createElement('div');
-    ReactDOM.render(<Appointment customer={customer} />, container);
+    customer = { firstName: 'Bob' };
+    render(<Appointment customer={customer} />);
     expect(container.textContent).toMatch('Bob');
   });
 });
