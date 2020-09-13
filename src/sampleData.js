46d89e88312fe3c2d34cab55ae38fd9ef79b764c
y_fukuda <colors92window@gmail.com>
2020-09-13 23:08:46 +0900

    mock data enhanced

--- src/sampleData.js
+++ src/sampleData.js
@@ -5,38 +5,83 @@ const at = (hours) => today.setHours(hours, 0);
 export const sampleAppointments = [
   {
     startsAt: at(9),
-    customer: { firstName: 'Alice', lastName: 'Sandoval'}
+    customer: {
+      firstName: 'Alice',
+      lastName: 'Sandoval',
+      phoneNumber: '555-105-8228',
+      stylist: 'Adam Turner',
+    },
   },
   {
     startsAt: at(10),
-    customer: { firstName: 'Bob', lastName: 'Sandoval'}
+    customer: {
+      firstName: 'Bob',
+      lastName: 'Sandoval',
+      phoneNumber: '555-453-3941',
+      stylist: 'Bella Byrd',
+    },
   },
   {
     startsAt: at(11),
-    customer: { firstName: 'Charles', lastName: 'Strickland'}
+    customer: {
+      firstName: 'Charles',
+      lastName: 'Strickland',
+      phoneNumber: '555-767-0068',
+      stylist: 'Christopher Knight',
+    },
   },
   {
     startsAt: at(12),
-    customer: { firstName: 'Daisy', lastName: 'Mejia'}
+    customer: {
+      firstName: 'Daisy',
+      lastName: 'Mejia',
+      phoneNumber: '555-118-6070',
+      stylist: 'Adam Turner',
+    },
   },
   {
     startsAt: at(13),
-    customer: { firstName: 'Edward', lastName: 'Palmer'}
+    customer: {
+      firstName: 'Edward',
+      lastName: 'Palmer',
+      phoneNumber: '555-873-4563',
+      stylist: 'Bella Byrd',
+    },
   },
   {
     startsAt: at(14),
-    customer: { firstName: 'Felicity', lastName: 'Alexander'}
+    customer: {
+      firstName: 'Felicity',
+      lastName: 'Alexander',
+      phoneNumber: '555-887-8174',
+      stylist: 'Christopher Knight',
+    },
   },
   {
     startsAt: at(15),
-    customer: { firstName: 'George', lastName: 'Mann'}
+    customer: {
+      firstName: 'George',
+      lastName: 'Mann',
+      phoneNumber: '555-664-7286',
+      stylist: 'Adam Turner',
+    },
   },
   {
     startsAt: at(16),
-    customer: { firstName: 'Hannah', lastName: 'Wise'}
+    customer: {
+      firstName: 'Hannah',
+      lastName: 'Wise',
+      phoneNumber: '555-666-1410',
+      stylist: 'Bella Byrd',
+    },
   },
   {
     startsAt: at(17),
-    customer: { firstName: 'Isaac', lastName: 'Thornton'}
+    customer: {
+      firstName: 'Isaac',
+      lastName: 'Thornton',
+      phoneNumber: '555-424-2540',
+      stylist: 'Christopher Knight',
+    },
   },
 ];
