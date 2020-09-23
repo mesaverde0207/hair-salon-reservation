1c4f4afd00dd46f54a437ca0876a2b5ab2a396fd
y_fukuda <colors92window@gmail.com>
2020-09-24 07:56:12 +0900

    CustomerForm includes the existing value for the first name

--- test/CustomerForm.test.js
+++ test/CustomerForm.test.js
@@ -11,6 +11,11 @@ describe('CustomerForm', () => {
   });
 
   const form = id => container.querySelector(`form[id="${id}"]`);
+  const expectToBeInputFieldOfTypeText = (formElement) => {
+    expect(formElement).not.toBeNull();
+    expect(formElement.tagName).toEqual('INPUT');
+    expect(formElement.type).toEqual('text');
+  };
 
   it('renders a form', () => {
     render(<CustomerForm />);
@@ -20,9 +25,12 @@ describe('CustomerForm', () => {
   it('renders the first name field as a text box', () => {
     render(<CustomerForm />);
     const field = form('customer').elements.firstName;
-    expect(field).not.toBeNull();
-    expect(field.tagName).toEqual('INPUT');
-    expect(field.type).toEqual('text');
+    expectToBeInputFieldOfTypeText(field);
   });
 
+  it('includes the existing value for the first name', () => {
+    render(<CustomerForm firstName="Alice" />);
+    const field = form('customer').elements.firstName;
+    expect(field.value).toEqual('Alice');
+  });
 });
