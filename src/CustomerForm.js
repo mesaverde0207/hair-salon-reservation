f3f84be43c2d004ac5dfa7efc752140741183fce
y_fukuda <colors92window@gmail.com>
2020-09-24 07:23:42 +0900

    CustomerForm renders the first name field as a text box

--- src/CustomerForm.js
+++ src/CustomerForm.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
-export const CustomerForm = () => {
-  return <form id="customer" />;
-};
+export const CustomerForm = () => (
+  <form id="customer">
+    <input type="text" name="firstName">
+    </input>
+  </form>
+);
